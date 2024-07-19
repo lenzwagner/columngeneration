f3209d804219f099a6589645152c077fdc33389d
Fritzcola <lenzwagner@users.noreply.github.com>
2024-07-19 10:44:35 +0200

    Single ones

--- Agg/loop_bam.py
+++ Agg/loop_bam.py
@@ -12,21 +12,20 @@ import pandas as pd
 eps_ls = [0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
 chi_ls = [2, 3, 4, 5, 6, 7, 8, 9, 10]
 T = list(range(1, 29))
-I = list(range(1, 51))
+I = list(range(1, 101))
 K = [1, 2, 3]
 
 # DataFrame
-results = pd.DataFrame(columns=['I', 'pattern', 'epsilon', 'chi', 'objval', 'lbound', 'iteration', 'undercover', 'undercover_norm', 'cons', 'cons_norm', 'perf', 'perf_norm', 'max_auto', 'min_auto', 'mean_auto', 'lagrange', 'undercover_n', 'undercover_norm_n', 'cons_n', 'cons_norm_n', 'perf_n', 'perf_norm_n', 'max_auto_n', 'min_auto_n', 'mean_auto_n', 'lagrange_n'])
+results = pd.DataFrame(columns=['I', 'pattern', 'epsilon', 'chi', 'objval', 'lbound', 'iteration', 'undercover', 'undercover_norm', 'cons', 'cons_norm', 'perf', 'perf_norm', 'max_auto', 'min_auto', 'mean_auto', 'lagrange'])
 
 # Times and Parameter
 time_Limit = 7200
 time_cg = 7200
 time_cg_init = 60
-time_cg_init_npm = 30
 
 # Datanames
 current_time = datetime.now().strftime('%Y-%m-%d_%H')
-file = f'study_results_mulit_variation_50_{current_time}'
+file = f'study_results_mulit_bam_{current_time}'
 file_name_csv = f'.{os.sep}results{os.sep}study{os.sep}{file}.csv'
 file_name_xlsx = f'.{os.sep}results{os.sep}study{os.sep}{file}.xlsx'
 
@@ -56,9 +55,6 @@ for epsilon in eps_ls:
         })
 
         # Column Generation
-        undercoverage_n, understaffing_n, perfloss_n, consistency_n, consistency_norm_n, undercoverage_norm_n, understaffing_norm_n, perfloss_norm_n, results_sc_n, results_r_n, autocorell_n, lagrangeB_n = column_generation_naive(data, demand_dict, 0, Min_WD_i, Max_WD_i, time_cg_init_npm, max_itr, output_len, chi,
-                                    threshold, time_cg, I, T, K, eps)
-
         undercoverage, understaffing, perfloss, consistency, consistency_norm, undercoverage_norm, understaffing_norm, perfloss_norm, results_sc, results_r, autocorell, final_obj, final_lb, itr, lagrangeB = column_generation_behavior(data, demand_dict, eps, Min_WD_i, Max_WD_i, time_cg_init, max_itr, output_len, chi,
                                     threshold, time_cg, I, T, K)
 
@@ -80,21 +76,10 @@ for epsilon in eps_ls:
             'max_auto': round(max(autocorell), 5),
             'min_auto': round(min(autocorell), 5),
             'mean_auto': round(np.mean(autocorell), 5),
-            'lagrange': lagrangeB,
-            'undercover_n': undercoverage_n,
-            'undercover_norm_n': undercoverage_norm_n,
-            'cons_n': consistency_n,
-            'cons_norm_n': consistency_norm_n,
-            'perf_n': perfloss_n,
-            'perf_norm_n': perfloss_norm_n,
-            'max_auto_n': round(max(autocorell_n), 5),
-            'min_auto_n': round(min(autocorell_n), 5),
-            'mean_auto_n': round(np.mean(autocorell_n), 5),
-            'lagrange_n': lagrangeB_n
+            'lagrange': lagrangeB
         }])
 
         results = pd.concat([results, result], ignore_index=True)
 
 results.to_csv(file_name_csv, index=False)
 results.to_excel(file_name_xlsx, index=False)
-
